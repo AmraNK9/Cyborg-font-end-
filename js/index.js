d5a094ed46f2cfd15c715ab15f599f4048104e41
AmaraNK9 <naymyo1310mdy@gmail.com>
2023-02-07 00:29:50 -0800

    first

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-const body = document.querySelector('body')
+const body = document.querySelector("body");
 const navLink = document.querySelectorAll(".nav-link");
-const iframe = document.querySelector('iframe')
-const nav = document.querySelector('nav')
+const iframe = document.querySelector("iframe");
+const nav = document.querySelector("nav");
 navLink.forEach((el) => {
   el.addEventListener("click", (el2) => {
     navLink.forEach((el) => {
@@ -11,20 +11,17 @@ navLink.forEach((el) => {
   });
 });
 
-
-setInterval(()=>{
-    if(localStorage.getItem('scroll-Dir')== 'Down'){
-        nav.classList.add('Hide-on-top')
-    }
-    else{
-        nav.classList.remove('Hide-on-top')
-    }
-},300)
+setInterval(() => {
+  if (localStorage.getItem("scroll-Dir") == "Down") {
+    nav.classList.add("Hide-on-top");
+  } else {
+    nav.classList.remove("Hide-on-top");
+  }
+}, 300);
 //browse section
-navLink[0].addEventListener('click',(e)=>{
-    iframe.src = 'Home.html'
-})
-navLink[1].addEventListener('click',(e)=>{
-    iframe.src = 'Browse.html'
-})
-
+navLink[0].addEventListener("click", (e) => {
+  iframe.src = "Home.html";
+});
+navLink[1].addEventListener("click", (e) => {
+  iframe.src = "Browse.html";
+});
